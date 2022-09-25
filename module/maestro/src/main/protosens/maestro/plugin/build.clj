8ee911778d0460c53d1168c11a9bd80ff61695ee
Adam Helins <adam@protosens.com>
2022-09-25 10:59:00 +0200

    Add docstrings for the Maestro `tools.build` plugin

--- module/maestro/src/main/protosens/maestro/plugin/build.clj
+++ module/maestro/src/main/protosens/maestro/plugin/build.clj
@@ -1,6 +1,14 @@
 (ns protosens.maestro.plugin.build
 
-  "Building jars and uberjars."
+  "Maestro plugin for `tools.build` focused on building jars and uberjars, key information being located
+   right in aliases.
+
+   Aims to provide enough flexibility so that it would cover a majority of use cases. Also extensible by
+   implementing methods for [[by-type]].
+
+   Main entry point is [[build]] and [[task]] offers a fast way of getting into it using Babashka.
+  
+   <!> `tools.build` is not imported and must be brought by the user."
 
   (:import (java.nio.file Files)
            (java.nio.file.attribute FileAttribute))
@@ -16,7 +24,7 @@
 
 (defn- -fail
 
-  ;;
+  ;; Notably used when a user argument is missing.
 
   [message]
 
@@ -28,7 +36,9 @@
 
 (defn- -resolve
 
-  ;;
+  ;; `tools.build` being brought by users, we use that little charade below with delays and
+  ;; `requiring-resolve` so that the namespace can be analyzed by `cljdoc` even without access
+  ;; to `tools.build` (which is, in addition, a Git dependency).
 
   [sym]
 
@@ -38,7 +48,7 @@
       (throw (Exception. "`tools.build` must be added to dependencies")))))
 
 
-;;;
+;;; Access to required `tools.build` functions
 
 
 (def ^:private -d*compile-clj
@@ -87,6 +97,8 @@
 
 (defn clean
 
+  "Deletes the file under `:maestro.plugin.build.path/output`."
+
   [ctx]
 
   (let [path (ctx :maestro.plugin.build.path/output)]
@@ -98,6 +110,8 @@
 
 
 (defn copy-src
+
+  "Copies source from `:maestro.plugin.build.path/src+` to `:maestro.plugin.build.path/class`."
 
   [ctx]
 
@@ -111,6 +125,10 @@
 
 
 (defn- -jar
+
+  ;; Prelude common to jarring and uberjarring.
+  ;;
+  ;; Prepares paths and a temporary directory for work.
 
   [arg+]
 
@@ -127,7 +145,8 @@
     (println "Using temporary directory for building:"
              dir-tmp)
     (-> (merge {:maestro.plugin.build/basis       (@-d*create-basis {:aliases required-alias+
-                                                                     :project "deps.edn"})
+                                                                     :project (or (arg+ :maestro/project)
+                                                                                  "deps.edn")})
                 :maestro.plugin.build.path/class  path-class
                 :maestro.plugin.build.path/src+   path-src+
                 :maestro.plugin.build.path/target dir-tmp}
@@ -138,6 +157,56 @@
 
 
 (defn jar
+
+  "Implementation for the `:jar` type in [[by-type]].
+
+   Alias data for the build alias must or may contain:
+
+   | Key                                    | Value                         | Mandatory? | Default       |
+   |----------------------------------------|-------------------------------|------------|---------------|
+   | `:maestro.plugin.build.alias/artifact` | Artifact alias (see below)    | Yes        | /             |
+   | `:maestro.plugin.build.path/output`    | Output path for the jar       | Yes        | /             |
+   | `:maestro.plugin.build.path/pom`       | Path to the template POM file | No         | `\"pom.xml\"` |
+
+   A POM file will be created if necessary but it is often best starting from one that hosts key information
+   that does not change from build to build like SCM, organization, etc.
+
+   The artifact alias is an alias representing your release in its `:extra-deps` and nothing else. This is
+   where the artifact name and version are extracted from. For instance, in this repository, `deps.edn` contains
+   this artifact alias related to `:module/maestro`:
+
+   ```clojure
+   {:release/maestro
+    {:extra-deps {com.protosens/maestro {:mvn/version \"x.x.x\"}}
+     ...}}
+   ```
+
+   This is useful so that other modules can require this one in 2 ways using profiles: one for local development,
+   one for their own releases. For instance:
+
+   ```clojure
+   {:module/another-module
+    {:maestro/require [{default :module/maestro
+                        release :release/maestro}]
+     ...}}
+   ``` 
+
+   To go even further, it is possible to run tests against a release installed locally or downloaded remotely.
+   This ensure that everything was built correctly beyond any doubt. For instance, in this repository, Maestro
+   is tested like this:
+
+   ```
+   clojure -M$( bb aliases:test :module/maestro )
+   ```
+
+   But the following one will run the Maestro test suite against the Maestro version from the local Maven cache
+   after downloading it from Clojars if necessary:
+
+   ```
+   clojure -M$( bb aliases:test '[release :release/maestro]' )
+   ```
+
+   Note: it is best activating the `release` alias when doing that sort of things."
 
   [arg+]
 
@@ -160,7 +229,8 @@
                     :class-dir path-class
                     :lib       artifact
                     :src-dirs  (ctx :maestro.plugin.build.path/src+)
-                    :src-pom   (ctx :maestro.plugin.build.path/pom)
+                    :src-pom   (or (ctx :maestro.plugin.build.path/pom)
+                                   "pom.xml")
                     :version   (version-map :mvn/version)})
     (println "Assemling jar to:"
              path-jar)
@@ -171,6 +241,22 @@
 
 
 (defn uberjar
+
+  "Implementation for the `:uberjar` type in [[by-type]].
+  
+   Alias data for the build alias must or contain:
+
+   | Key                                      | Value                         | Mandatory? |
+   |------------------------------------------|-------------------------------|------------|
+   | `:maestro.plugin.build.path/exclude`     | Paths to exclude              | No         |
+   | `:maestro.plugin.build.path/output`      | Output path for the uberjar   | Yes        |
+   | `:maestro.plugin.build.uberjar/compiler` | Clojure compiler options      | No         |
+   | `:maestro.plugin.build.uberjar/main`     | Namespace containing `-main`  | Yes        |   
+
+   Clojure compiler options like activating direct linking are [described here](https://clojure.org/reference/compilation#_compiler_options).
+  
+   It is often useful providing the exclusion paths globally as a top-level key-value in `deps.edn` rather than duplicating it in every alias.
+   to build."
 
   [arg+]
 
@@ -184,7 +270,7 @@
     (println "Compiling" (ctx :maestro.plugin.build/alias))
     (@-d*compile-clj {:basis        basis
                       :class-dir    path-class
-                      :compile-opts (ctx :maestro.uberjar/compiler)
+                      :compile-opts (ctx :maestro.plugin.build.uberjar/compiler)
                       :src-dirs     (ctx :maestro.plugin.build.path/src+)})
     (println "Assembling uberjar to: %s"
              path-uberjar)
@@ -200,6 +286,17 @@
 
 
 (defmulti by-type
+
+  "Called by [[build]] after some initial preparation.
+   Dispatches on `:maestro.build.plugin/type` to carry out the actual build steps.
+
+   Supported types are:
+
+   | Type       | See         |
+   |------------|-------------|
+   | `:jar`     | [[jar]]     |
+   | `:uberjar` | [[uberjar]] |"
+
   :maestro.plugin.build/type)
 
 
@@ -233,10 +330,16 @@
   (uberjar arg+))
 
 
-;;;
+;;; Entry points
 
 
 (defn build
+
+  "Given a map with an alias to build under `:maestro.plugin.build/alias`, computes all required aliases
+   after activating the `release` profile.
+
+   Merges the contents with `deps.edn` (or the path mentioned under `:maestro/project`), the alias data of
+   the alias to build and the given `arg+`. Then, passes everything to [[by-type]]."
 
   [arg+]
 
@@ -260,6 +363,17 @@
 
 
 (defn task
+
+  "Convenient way of calling [[build]] by providing an alias to build.
+   
+   Requires at least the alias under which Maestro and `tools.build` are imported. Alias to build is read
+   as first command line argument if not provided explicitly.
+  
+   Useful as a Babashka task. For instance, in this repository, the jar for Maestro is built like this:
+
+   ```
+   bb build :module/maestro
+   ```"
 
 
   ([alias-maestro]
