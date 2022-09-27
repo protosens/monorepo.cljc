39baa589770a73fa8019f601fb2ceab02cb8e60b
Adam Helins <adam@protosens.com>
2022-09-27 17:51:20 +0200

    Return feedback on generating `deps.edn` files for gitlibs
    Returned value is a map with aliases pointing to some information that
    might be useful to know (e.g. required aliases).

--- module/maestro/src/main/clj/protosens/maestro/git/lib.clj
+++ module/maestro/src/main/clj/protosens/maestro/git/lib.clj
@@ -20,17 +20,69 @@
 
 (defn write-deps-edn
 
-  [root-dir deps-edn]
+  [path deps-edn]
 
-  (let [path (str root-dir
-                  "/deps.edn")]
-    (with-open [writer (java.io/writer path)]
-      (binding [*out* writer]
-        (println ";; This is a generated file allowing this module to be consumed")
-        (println ";; as a git dependency via `:deps/root`.")
-        (println ";;")
-        (pprint/pprint deps-edn)))
-    path))
+  (with-open [writer (java.io/writer path)]
+    (binding [*out* writer]
+      (println ";; This is a generated file allowing this module to be consumed")
+      (println ";; as a git dependency via `:deps/root`.")
+      (println ";;")
+      (pprint/pprint deps-edn)))
+  path)
+
+
+
+(defn prepare-deps-edn
+
+  [basis alias]
+
+  (let [alias->data (basis :aliases)
+        data        (alias->data alias)
+        root-dir    (data :maestro/root)
+        required    (-> basis
+                        (assoc :maestro/alias+
+                               [alias])
+                        ($.maestro/search)
+                        (:maestro/require))
+        deps-edn    (reduce (fn [deps-edn alias-required]
+                              (let [data-required (alias->data alias-required)
+                                    deps-edn-2    (update deps-edn
+                                                          :deps
+                                                          merge
+                                                          (data-required :extra-deps))]
+                                (if (and (not= alias-required
+                                               alias)
+                                         (gitlib? data-required))
+                                  (assoc-in deps-edn-2
+                                            [:deps
+                                             (data-required :maestro.git.lib/name)]
+                                            {:local/root (str (fs/relativize root-dir
+                                                                             (data-required :maestro/root)))})
+                                  (update deps-edn-2
+                                          :paths
+                                          into
+                                          (map (fn [path]
+                                                 (when-not (string/starts-with? path
+                                                                                root-dir)
+                                                   (throw (ex-info "Path to add does not belong"
+                                                                   {:alias/git-lib  alias
+                                                                    :alias/required alias-required
+                                                                    :path           path
+                                                                    :root           root-dir})))
+                                                 (str (fs/relativize root-dir
+                                                                     path))))
+                                          (data-required :extra-paths)))))
+                            {:deps  (sorted-map)
+                             :paths #{}}
+                            required)]
+    {:maestro/require               required
+     :maestro.git.lib/deps.edn      (update deps-edn
+                                            :paths
+                                            (comp vec
+                                                  sort))
+     :maestro.git.lib.path/deps.edn (str root-dir
+                                         "/deps.edn")}))
+
 
 
 (defn gen-deps
@@ -49,56 +101,22 @@
                          (assoc :maestro/alias+
                                 (vec (keys alias->data)))
                          ($.maestro/search))
-         required    (filterv (fn [alias]
+         gitlib+     (filterv (fn [alias]
                                 (let [data (alias->data alias)]
                                   (and (gitlib? data)
                                        (or (data :maestro/root)
                                            (throw (Exception. (str "Missing root in alias data: "
                                                                    alias)))))))
                               (basis-3 :maestro/require))]
-     (doseq [alias required]
-       (let [data     (alias->data alias)
-             root-dir (data :maestro/root)]
-         (write-deps-edn
-           root-dir
-           (-> (reduce (fn [deps-edn alias-required]
-                         (let [data-required (alias->data alias-required)
-                               deps-edn-2    (update deps-edn
-                                                     :deps
-                                                     merge
-                                                     (data-required :extra-deps))]
-                           (if (and (not= alias-required
-                                          alias)
-                                    (gitlib? data-required))
-                             (assoc-in deps-edn-2
-                                       [:deps
-                                        (data-required :maestro.git.lib/name)]
-                                       {:local/root (str (fs/relativize root-dir
-                                                                        (data-required :maestro/root)))})
-                             (update deps-edn-2
-                                     :paths
-                                     into
-                                     (map (fn [path]
-                                            (when-not (string/starts-with? path
-                                                                           root-dir)
-                                              (throw (ex-info "Path to add does not belong"
-                                                              {:alias/git-lib  alias
-                                                               :alias/required alias-required
-                                                               :path           path
-                                                               :root           root-dir})))
-                                            (str (fs/relativize root-dir
-                                                                path))))
-                                     (data-required :extra-paths)))))
-                       {:deps  (sorted-map)
-                        :paths #{}}
-                       (-> basis-2
-                           (assoc :maestro/alias+
-                                  [alias])
-                           ($.maestro/search)
-                           (:maestro/require)))
-               (update :paths
-                       (comp vec
-                             sort)))))))))
+     (into {}
+           (map (fn [alias]
+                  (let [prepared (prepare-deps-edn basis-2
+                                                   alias)]
+                    (write-deps-edn (prepared :maestro.git.lib.path/deps.edn)
+                                    (prepared :maestro.git.lib/deps.edn))
+                    [alias (dissoc prepared
+                                   :maestro.git.lib/deps.edn)])))
+           gitlib+))))
 
 
 (comment
