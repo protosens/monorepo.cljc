6f4f26366bc3e22066f92a367659c69d62d32bf6
Adam Helins <adam@protosens.com>
2022-10-01 10:17:13 +0200

    Introduce `:module/git`
    A nascent collection of one-liners for shelling out common Git
    operations. Compatible with Babashka and the JVM.

---
+++ module/git/src/main/clj/protosens/git.clj
@@ -0,0 +1,216 @@
+(ns protosens.git
+
+  (:require [babashka.process :as bb.process]
+            [clojure.string   :as string]
+            [protosens.txt    :as $.txt]))
+
+
+;;;;;;;;;; Master command
+
+
+(defn exec
+
+
+  ([arg+]
+
+   (exec arg+
+         nil))
+
+
+  ([arg+ option+]
+
+   (let [{:keys [err
+                 exit
+                 out]} (bb.process/sh (cons (or (:command option+)
+                                                "git")
+                                            arg+)
+                                      option+)]
+     (cond->
+       {:exit exit}
+       ;;
+       (seq err)
+       (assoc :err
+              err)
+       ;;
+       (seq out)
+       (assoc :out
+              out)))))
+
+
+;;;;;;;;;; Quick commands
+
+
+(defn add
+
+
+  ([]
+
+   (add nil))
+
+
+  ([path+]
+
+   (add path+
+        nil))
+
+
+  ([path+ option+]
+
+   (-> (exec (if (seq path+)
+               (cons "add"
+                     path+)
+               ["add" "."])
+             option+)
+       (:exit)
+       (zero?))))
+
+
+
+(defn branch
+
+
+  ([]
+
+   (branch nil))
+
+
+  ([option+]
+
+   (-> (exec ["branch" "--show-current"]
+             option+)
+       (:out)
+       (string/trimr))))
+
+
+
+(defn branch+
+
+
+  ([]
+
+   (branch+ nil))
+
+
+  ([option+]
+
+   (-> (exec ["branch"]
+             option+)
+       (:out)
+       (string/split-lines)
+       (->> (map (fn [^String branch]
+                   (.substring branch
+                               2
+                               (count branch))))))))
+
+
+
+(defn change?
+
+  ;; Anything different in versioned files, staged or not.
+  
+  ([]
+
+   (change? nil))
+
+
+  ([option+]
+
+   (-> (exec ["diff-index" "--quiet" "HEAD"]
+             option+)
+       (:exit)
+       (pos?))))
+
+
+
+(defn clean?
+
+
+
+  ([]
+
+   (clean? nil))
+
+
+  ([option+]
+
+   (-> (exec ["status" "--porcelain"]
+             option+)
+       (:out)
+       (empty?))))
+
+
+
+(defn commit
+
+
+  ([message]
+
+   (commit message
+           nil))
+
+
+  ([message option+]
+
+   (-> (exec ["commit" "-m" ($.txt/realign message)]
+             option+)
+       (:exit)
+       (zero?))))
+
+
+
+(defn last-sha
+
+
+  ([]
+
+   (last-sha 0))
+
+  
+  ([i]
+
+   (last-sha i
+             nil))
+
+
+  ([i option+]
+
+   (-> (exec ["rev-parse" (str "HEAD^"
+                               (or i
+                                   0))]
+             option+)
+       (:out)
+       (string/trimr))))
+
+
+
+(defn unstaged?
+
+  
+  ([]
+
+   (unstaged? nil))
+
+
+  ([option+]
+
+   (-> (exec ["diff" "--quiet"]
+             option+)
+       (:exit)
+       (pos?))))
+
+
+
+(defn version
+
+
+  ([]
+
+   (version nil))
+
+
+  ([option+]
+
+   (-> (exec ["--version"]
+             option+)
+       (:out)
+       (string/trimr))))
