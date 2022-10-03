bcc77ad0d519238f333e6426f3242c41d1988312
Adam Helins <adam@protosens.com>
2022-10-03 15:08:02 +0200

    Add tests for `$.process/run`
    And related utilities.

--- module/process/src/test/clj/protosens/test/process.clj
+++ module/process/src/test/clj/protosens/test/process.clj
@@ -9,10 +9,25 @@
 ;;;;;;;;;;
 
 
+(T/deftest run
+
+  (let [process ($.process/run ["echo" "foo"])]
+
+    (T/is (zero? ($.process/exit-code process)))
+
+    (T/is ($.process/success? process))
+
+    (T/is (nil? ($.process/err process)))
+
+    (T/is (= "foo"
+             ($.process/out process)))))
+
+
+
 (T/deftest shell
 
   (let [process ($.process/shell ["echo" "This is printed from a test."])]
 
     (T/is (zero? ($.process/exit-code process)))
-    
+
     (T/is ($.process/success? process))))
