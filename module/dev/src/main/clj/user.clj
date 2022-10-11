0cad2113ac8f92b0efbad9dd3ec376fe6e2c60c2
Adam Helins <adam@protosens.com>
2022-10-11 11:06:41 +0200

    Remove prelude dev namespace

--- module/dev/src/main/clj/user.clj
+++ module/dev/src/main/clj/user.clj
@@ -2,14 +2,29 @@
 
   "Loaded automatically."
 
-  (:require [protosens.dev.prelude :as $.dev.prelude]))
+  (:require [protosens.namespace :as $.namespace]
+            [protosens.symbol    :as $.symbol]))
+
 
 
 ;;;;;;;;;;
 
 
-(def req
-     $.dev.prelude/req)
+(defn req
+
+  "Requires all Protosens namespace available on the classpath."
+
+  []
+
+  ($.namespace/require-cp-dir+
+    (fn [nmspace]
+      (when ($.symbol/starts-with? nmspace
+                                   'protosens.)
+        [nmspace
+         :as
+         ($.symbol/replace-first nmspace
+                                 'protosens
+                                 '$)]))))
 
 
 
