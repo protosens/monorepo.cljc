81356fd61d1b1429fd484ba781aa590da330e9f0
Adam Helins <adam@protosens.com>
2023-12-26 08:37:25 +0100

    Print required aliases in `$.maestro.plugin.bb/run`

--- module/maestro.plugin.bb/src/main/clj/protosens/maestro/plugin/bb.clj
+++ module/maestro.plugin.bb/src/main/clj/protosens/maestro/plugin/bb.clj
@@ -2,6 +2,7 @@
 
   (:require [clojure.java.io    :as C.java.io]
             [clojure.pprint     :as C.pprint]
+            [clojure.string     :as C.string]
             [protosens.edn.read :as $.edn.read]
             [protosens.maestro  :as $.maestro]))
 
@@ -21,14 +22,26 @@
   (println "- Creating `bb.edn` from `bb.maestro.edn` and `deps.maestro.edn`")
   (println (format "- Computing everything required for alias `%s`"
                    alias))
-  (let [merged (merge ($.edn.read/file "bb.maestro.edn")
-                      (-> ($.maestro/-run (str alias)
-                                          nil)
+  (let [sorted ($.maestro/-run (str alias)
+                               nil)
+        bb-edn (merge ($.edn.read/file "bb.maestro.edn")
+                      (-> sorted
                           (::$.maestro/result)
                           (select-keys [:deps
                                         :paths])))]
-    (with-open [file (C.java.io/writer "bb.edn")]
-      (C.pprint/pprint merged
-                       file)))
-  (println "- `bb.edn` is ready")
+    (if (= bb-edn
+           ($.edn.read/file "bb.edn"))
+      (println "- Done, nothing changed")
+      (do
+        (with-open [file (C.java.io/writer "bb.edn")]
+          (C.pprint/pprint bb-edn
+                       file))
+        (println "- Prepared `bb.edn` with aliases:")
+        (doseq [[alias
+                 depth] (sorted ::$.maestro/path)]
+          (println (format "%s%s"
+                           (C.string/join (repeat (inc depth)
+                                                  "  "))
+                           alias)))
+        (println "- Done, `bb.edn` is ready"))))
   nil)
