9dd8bece7d39089c83fa80e63451b0716a5afe3b
Adam Helins <adam@protosens.com>
2023-12-26 08:54:52 +0100

    Refactor `:module/maestro.plugin.bb`

--- module/maestro.plugin.bb/src/main/clj/protosens/maestro/plugin/bb.clj
+++ module/maestro.plugin.bb/src/main/clj/protosens/maestro/plugin/bb.clj
@@ -7,7 +7,54 @@
             [protosens.maestro  :as $.maestro]))
 
 
-;;;;;;;;;;
+;;;;;;;;;; Private side effects
+
+
+(defn- -print-path
+
+  [path]
+
+  (println "- Done, prepared `bb.edn` with aliases:")
+  (println)
+  (doseq [[alias
+           depth] path]
+    (println (format "%s%s"
+                     (C.string/join (repeat (inc depth)
+                                            "  "))
+                     alias))))
+
+
+
+(defn- -write-file
+
+  [bb-edn path]
+
+  (println "- Writing new `bb.edn`")
+  (with-open [file (C.java.io/writer "bb.edn")]
+    (C.pprint/pprint bb-edn
+                 file))
+  (-print-path path))
+
+
+;;;;;;;;;; Task
+
+
+(defn ^:no-doc -run
+
+  [alias bb-edn bb-maestro-edn deps-maestro-edn]
+
+  (let [sorted     ($.maestro/-run (str alias)
+                                   deps-maestro-edn)
+        bb-edn-new (merge bb-maestro-edn
+                          (-> sorted
+                              (::$.maestro/result)
+                              (select-keys [:deps
+                                            :paths])))]
+    (when-not (= bb-edn-new
+                 bb-edn)
+      [bb-edn-new
+       (sorted ::$.maestro/path)])))
+
 
 
 (defn run
@@ -22,26 +69,11 @@
   (println "- Creating `bb.edn` from `bb.maestro.edn` and `deps.maestro.edn`")
   (println (format "- Computing everything required for alias `%s`"
                    alias))
-  (let [sorted ($.maestro/-run (str alias)
-                               nil)
-        bb-edn (merge ($.edn.read/file "bb.maestro.edn")
-                      (-> sorted
-                          (::$.maestro/result)
-                          (select-keys [:deps
-                                        :paths])))]
-    (if (= bb-edn
-           ($.edn.read/file "bb.edn"))
-      (println "- Done, nothing changed")
-      (do
-        (with-open [file (C.java.io/writer "bb.edn")]
-          (C.pprint/pprint bb-edn
-                       file))
-        (println "- Prepared `bb.edn` with aliases:")
-        (doseq [[alias
-                 depth] (sorted ::$.maestro/path)]
-          (println (format "%s%s"
-                           (C.string/join (repeat (inc depth)
-                                                  "  "))
-                           alias)))
-        (println "- Done, `bb.edn` is ready"))))
-  nil)
+  (if-some [[bb-edn
+             path]  (-run alias
+                          ($.edn.read/file "bb.edn")
+                          ($.edn.read/file "bb.maestro.edn")
+                          ($.edn.read/file "deps.maestro.edn"))]
+    (-write-file bb-edn
+                 path)
+    (println "- Done, nothing changed")))
