86e0dab028e3f6e7494fd42267531e8261f3c427
Adam Helins <adam@protosens.com>
2024-01-02 15:38:07 +0100

    Update `:module/maestro.plugin.clj-kondo` to new Maestro version

--- module/maestro.plugin.clj-kondo/src/main/clj/protosens/maestro/plugin/clj_kondo.clj
+++ module/maestro.plugin.clj-kondo/src/main/clj/protosens/maestro/plugin/clj_kondo.clj
@@ -4,68 +4,120 @@
   
    Assumes `clj-kondo` is installed and available in the shell."
 
-  (:require [protosens.maestro   :as $.maestro]
-            [protosens.classpath :as $.classpath]
-            [protosens.process   :as $.process]))
+  (:import (java.io StringWriter))
+  (:require [babashka.deps            :as bb.deps]
+            [pod.borkdude.clj-kondo   :as clj-kondo]
+            [protosens.maestro.plugin :as $.maestro.plugin]
+            [protosens.term.style     :as $.term.style]))
 
 
-;;;;;;;;;;
+;;;;;;;;;; Private helpers
 
 
-(defn prepare
-
-  "Prepares the Clj-kondo cache by linting all dependencies and copying configuration files.
-  
-   Should be called prior to [[lint]]ing for the first time and on dependency updates.
-  
-   Returns `true` in case of success."
+(defn -classpath
 
   []
 
-  (let [cp (-> ($.maestro/create-basis)
-               (:aliases)
-               (keys)
-               ($.classpath/compute))]
-    (-> ($.process/shell ["clj-kondo" "--parallel" "--copy-configs" "--lint" cp "--dependencies"])
-        ($.process/success?))))
+  (let [writer (StringWriter.)]
+    (binding [*out* writer]
+      (bb.deps/clojure ["-Spath"]))
+    [(str writer)]))
 
+
+;;;;;;;;;; Tasks
 
 
 (defn lint
 
-  "Lints the whole repository by extracting `:extra-paths` from aliases.
-
-   Options may be:
-
-   | Key            | Value                                                       |
-   |----------------|-------------------------------------------------------------|
-   | `:path-filter` | Predicate function deciding whether a path should be linted |
-
-   Returns `true` in case of success."
-        
 
   ([]
 
    (lint nil))
 
 
-  ([option+]
+  ([deps-edn]
 
-   (-> ($.process/shell (concat ["clj-kondo" "--parallel" "--lint"]
-                                (let [basis       ($.maestro/create-basis)
-                                      path-filter (:path-filter option+)]
-                                  (if path-filter
-                                    (reduce-kv (fn [acc alias data]
-                                                 (reduce (fn [acc-2 path]
-                                                           (cond->
-                                                             acc-2
-                                                             (path-filter alias
-                                                                          path)
-                                                             (conj path)))
-                                                         acc
-                                                         (:extra-paths data)))
-                                               []
-                                               (basis :aliases))
-                                    (mapcat :extra-paths
-                                            (vals (:aliases ($.maestro/create-basis))))))))
-       ($.process/success?))))
+   ($.maestro.plugin/intro "maestro.plugin.clj-kondo/lint")
+   ($.maestro.plugin/safe
+     (delay
+       ($.maestro.plugin/step "Linting `:paths`")
+       (let [deps-edn-2 (or deps-edn
+                            ($.maestro.plugin/read-file-edn "deps.edn"))
+             path+      (deps-edn-2 :paths)
+             analysis   (clj-kondo/run! {:lint     path+
+                                         :parallel true})
+             summary    (analysis :summary)
+             n-error    (summary :error)
+             n-warning  (summary :warning)]
+         (if (or (> n-error
+                    0)
+                 (> n-warning
+                    0))
+             (do
+               ($.maestro.plugin/step "Files to fix:")
+               (doseq [[file
+                        data] (sort-by first
+                                       (group-by :filename
+                                                 (analysis :findings)))]
+                 ($.maestro.plugin/step 1
+                                        (str $.term.style/bold
+                                             file
+                                             $.term.style/reset))
+                 (doseq [data-2  (sort (fn [data-a data-b]
+                                           (let [row-a (data-a :row)
+                                                 row-b (data-b :row)]
+                                             (if (= row-a
+                                                    row-b)
+                                               (compare (data-a :col)
+                                                        (data-b :col))
+                                               (compare row-a
+                                                        row-b))))
+                                         data)]
+                   ($.maestro.plugin/step 2
+                                          (format "%s%s:%s [%s]%s %s"
+                                                  $.term.style/fg-red
+                                                  (data-2 :row)
+                                                  (data-2 :col)
+                                                  (name (data-2 :level))
+                                                  $.term.style/reset
+                                                  (data-2 :message)))))
+               ($.maestro.plugin/fail (format "%d error%s, %d warning%s"
+                                              n-error
+                                              (if (> n-error
+                                                     1)
+                                                "s"
+                                                "")
+                                              n-warning
+                                              (if (> n-warning
+                                                     1)
+                                                "s"
+                                                ""))))
+           ($.maestro.plugin/done "Everything is fine")))))))
+
+
+
+(defn prepare
+
+
+  ([]
+
+   (prepare nil))
+
+
+  ([deps-edn]
+
+   ($.maestro.plugin/intro "maestro.plugin.clj-kondo/prepare")
+   ($.maestro.plugin/safe
+     (delay
+       ($.maestro.plugin/step "Preparing everything for Clj-kondo")
+       (let [deps-edn-2 (or deps-edn
+                            ($.maestro.plugin/read-file-edn "deps.edn"))
+
+             _          ($.maestro.plugin/step "Computing classpath based on `deps.edn`")
+             cp         (-classpath)]
+         ($.maestro.plugin/step "Running analysis")
+         (clj-kondo/run! {:copy-configs true
+                          :dependencies true
+                          :lint         cp
+                          :parallel     true})
+         ($.maestro.plugin/done "Clj-kondo is ready"))))))
