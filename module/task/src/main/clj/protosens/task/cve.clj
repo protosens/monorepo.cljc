bedcbba71e27af6db733bf36b347f9ef91fccbc1
Adam Helins <adam@protosens.com>
2024-01-08 14:34:22 +0100

    Update `:ext/nvd-clojure`
    And make the `deps:cve` task behave more like a Maestro plugin.

--- module/task/src/main/clj/protosens/task/cve.clj
+++ module/task/src/main/clj/protosens/task/cve.clj
@@ -1,22 +1,57 @@
 (ns protosens.task.cve
 
-  (:require [protosens.classpath :as $.classpath]
-            [protosens.process   :as $.process]))
+  (:require [babashka.fs              :as bb.fs]
+            [protosens.classpath      :as $.classpath]
+            [protosens.maestro.plugin :as $.maestro.plugin]
+            [protosens.process        :as $.process]))
 
 
-;;;;;;;;;;
+;;;;;;;;;; Private
 
 
-(defn run
+(defn- -classpath
 
   []
 
-  (let [cp ($.classpath/compute)]
-    @($.process/shell ["clojure"
-                       "-T:ext/nvd-clojure"
-                       "nvd.task/check"
-                       ":classpath"
-                       (format "\"%s\""
-                               cp)
-                       ":config-filename"
-                       "\"nvd/config.edn\""])))
+  ($.maestro.plugin/step "Computing classpath to check from current `deps.edn`")
+  ($.classpath/compute))
+
+
+
+(defn- -nvd-token
+
+  []
+
+  (let [path "private/nvd-token.txt"]
+    ($.maestro.plugin/step (format "Retrieving NVD API token from `%s`"
+                                   path))
+    (when-not (bb.fs/exists? path)
+      ($.maestro.plugin/fail (format "NVD API token must be in `%s`"
+                                     path)))
+    (or (not-empty (slurp path))
+        ($.maestro.plugin/fail (format "NVD API token from `%s` is empty?"
+                                       path)))))
+
+
+;;;;;;;;;; Public
+
+
+(defn check
+
+  []
+
+  ($.maestro.plugin/intro "protosens.task.cve/check")
+  ($.maestro.plugin/safe
+    (delay
+      (let [cp        (-classpath)
+            nvd-token (-nvd-token)]
+        ($.maestro.plugin/done "Everything is ready, will now run `:ext/nvd-clojure`")
+        @($.process/shell ["clojure"
+                           "-T:ext/nvd-clojure"
+                           "nvd.task/check"
+                           ":classpath"
+                           (format "\"%s\""
+                                   cp)
+                           ":config-filename"
+                           "\"nvd/config.edn\""]
+                          {:extra-env {"NVD_API_TOKEN" nvd-token}})))))
