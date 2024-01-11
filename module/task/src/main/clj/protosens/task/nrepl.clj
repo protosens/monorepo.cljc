0d31197bb11a706d565cf3428289a474b6a35126
Adam Helins <adam@protosens.com>
2024-01-11 15:33:04 +0100

    Improve `nrepl` task

--- module/task/src/main/clj/protosens/task/nrepl.clj
+++ module/task/src/main/clj/protosens/task/nrepl.clj
@@ -1,15 +1,26 @@
 (ns protosens.task.nrepl
 
-  (:require [protosens.task :as $.task]))
+  (:require [protosens.deps.edn       :as $.deps.edn]
+            [protosens.maestro.plugin :as $.maestro.plugin]
+            [protosens.task           :as $.task]))
 
 
 ;;;;;;;;;;
 
 
-(defn run
+(defn server
 
   []
 
-  ($.task/shell (concat ["clojure"
-                            "-M:ext/nrepl"]
-                        *command-line-args*)))
+  ($.maestro.plugin/intro "protosens.task.nrepl/server")
+  ($.maestro.plugin/safe
+    (delay
+      ($.maestro.plugin/step "Starting NREPL server")
+      (when-not (-> ($.deps.edn/read)
+                    (:aliases)
+                    (contains? :ext/nrepl))
+        ($.maestro.plugin/fail "`deps.edn` does not contain `:ext/nrepl`, are you in `:dev` mode?"))
+      (println)
+      ($.task/shell (concat ["clojure"
+                                "-M:ext/nrepl"]
+                            *command-line-args*)))))
