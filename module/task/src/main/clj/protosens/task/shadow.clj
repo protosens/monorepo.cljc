f9d33f71b9baa85b4113c34e57e5e24f8b869ae6
Adam Helins <adam@protosens.com>
2024-01-11 15:58:58 +0100

    Improve termin output of `shadow` task

--- module/task/src/main/clj/protosens/task/shadow.clj
+++ module/task/src/main/clj/protosens/task/shadow.clj
@@ -1,6 +1,8 @@
 (ns protosens.task.shadow
 
-  (:require [protosens.process :as $.process]))
+  (:require [protosens.deps.edn       :as $.deps.edn]
+            [protosens.maestro.plugin :as $.maestro.plugin]
+            [protosens.process        :as $.process]))
 
 
 ;;;;;;;;;;
@@ -16,11 +18,30 @@
 
   ([arg+]
 
+   (when-not (-> ($.deps.edn/read)
+                 (:aliases)
+                 (contains? :ext/shadow-cljs))
+     ($.maestro.plugin/fail "`deps.edn` does not contain `:ext/shadow-cljs`"))
    (let [exit-code (-> (protosens.process/shell (concat ["clojure"
                                                          "-M:ext/shadow-cljs"]
                                                         arg+
                                                         *command-line-args*))
                        ($.process/exit-code))]
      (when (not (zero? exit-code))
-       (System/exit exit-code))
+       ($.maestro.plugin/fail (format "Shadow-CLJS terminated with code %d"
+                                      exit-code)))
      exit-code)))
+
+
+
+(defn task
+
+  []
+
+  ($.maestro.plugin/intro "protosens.task.shadow/task")
+  ($.maestro.plugin/safe
+    (delay
+      ($.maestro.plugin/step "Running Shadow-CLJS with command-line arguments")
+      (println)
+      (run)
+      ($.maestro.plugin/done "Shadow-CLJS terminated normally"))))
